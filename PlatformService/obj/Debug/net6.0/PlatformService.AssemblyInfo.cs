f5618157c8b1e086edffce78abd8f4004e5cb656
Joseph <yosephassefa85@gmail.com>
2023-07-26 15:03:56 +0300

    docker file added

--- PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bf518400-1725-4547-962c-7739311b7852")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
